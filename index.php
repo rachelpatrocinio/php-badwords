e413e2fbcde184ac62f7d9b52652567299d69501
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-05-03 15:58:20 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-badwords</title>
+</head>
+<body>
+
+<form action="./server.php">
+    <textarea name="text" id="text" cols="30" rows="10"></textarea>
+    <button>INVIA</button>
+</form>
+    
+</body>
+</html>
---
+++ server.php
@@ -0,0 +1,7 @@
+<?php
+// var_dump($_GET);
+$text = $_GET['text'];
+?>
+
+<h3>ORIGINAL TEXT</h3>
+<p><?php echo $text; ?></p>
